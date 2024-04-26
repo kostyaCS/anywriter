adf730a837272cbb02c0b9a7c1bcafba649e78cb
kostyantin <kostyantin1408@gmail.com>
2024-04-26 18:25:39 +0300

    added comments

--- src/screens/CheckWork.js
+++ src/screens/CheckWork.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import { useParams } from 'react-router-dom';
-import { ref, onValue } from "firebase/database";
+import { ref, update, onValue } from "firebase/database";
 import { rtdb } from "../firebase";
 import { useNavigate} from "react-router-dom";
 
 const CheckWork = () => {
     const navigate = useNavigate();
     const { workId } = useParams();
+    const [comment, setComment] = useState("");
     const [work, setWork] = useState(null);
 
     useEffect(() => {
@@ -25,10 +26,55 @@ const CheckWork = () => {
         return <StyledDiv>Loading...</StyledDiv>;
     }
 
+    const submitForm = (event) => {
+        event.preventDefault();
+
+        let updates = {};
+
+        const newReview = comment;
+        const currentReviews = work.reviews || [];
+        const newReviewKey = currentReviews.length.toString();
+
+        updates[`/works/${workId}/reviews/${newReviewKey}`] = newReview;
+        update(ref(rtdb), updates).then(() => {
+            console.log('Review added successfully!');
+
+            const updatedReviews = [...currentReviews, newReview];
+            setWork({ ...work, reviews: updatedReviews });
+            setComment('');
+        }).catch(error => {
+            console.error('Error adding review: ', error);
+        });
+    };
+
+
     return (
         <StyledContainer>
             <Title>{work.title}</Title>
             <StyledDiv dangerouslySetInnerHTML={{ __html: work.content }} />
+            <AddCommentSection>
+                <Title>
+                    Add your comment
+                </Title>
+                <CommentForm onSubmit={submitForm}>
+                    <textarea
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        required
+                    />
+                    <StyledButton type="submit">Add comment</StyledButton>
+                </CommentForm>
+            </AddCommentSection>
+            {work.reviews && work.reviews.length > 0 && (
+                <ReviewsSection>
+                    <h2>Reviews</h2>
+                    {work.reviews.map((review, index) => (
+                        <ReviewItem key={index}>
+                            <p>{review}</p>
+                        </ReviewItem>
+                    ))}
+                </ReviewsSection>
+            )}
             <StyledButton onClick={handleBackClick}>Back</StyledButton>
         </StyledContainer>
     );
@@ -36,6 +82,21 @@ const CheckWork = () => {
 }
 
 export default CheckWork;
+
+const ReviewsSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin-bottom: 50px;
+`;
+
+const ReviewItem = styled.div`
+    border: 1px solid black;
+    width: 50%;
+`;
 
 
 const StyledContainer = styled.div`
@@ -45,10 +106,27 @@ const StyledContainer = styled.div`
     align-items: center;
 `;
 
+const AddCommentSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    justify-content: center;
+    align-items: center;
+`;
+
 const Title = styled.div`
     
 `;
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+    margin-bottom: 50px;
+`;
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+    margin-bottom: 50px;
+`;
+
+const CommentForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
